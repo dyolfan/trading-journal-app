f921a1a560064cfc8138d5011cf68952c353b2db
kpu <dyolfan@gmail.com>
2023-11-04 21:22:57 +0200

    Add eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		jest: true,
+	},
+	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: [".eslintrc.{js,cjs}"],
+			parserOptions: {
+				sourceType: "script",
+			},
+		},
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["@typescript-eslint", "react-hooks", "prettier"],
+	rules: {
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+		"react/react-in-jsx-scope": "off",
+		camelcase: "error",
+		"spaced-comment": "error",
+		"no-duplicate-imports": "error",
+	},
+	settings: {
+		"import/resolver": {
+			typescript: {},
+		},
+	},
+};
